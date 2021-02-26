603819d13ef53b9f96815e97fee818320bfe8ec7
minoh0201 <minoh@vt.edu>
2021-02-26 13:19:21 -0500

    Changing aug_rate to 0.5 for FS-DBG-AE

--- main.py
+++ main.py
@@ -85,9 +85,9 @@ if __name__ == "__main__":
         else:
             dims=[128, 64]
 
-        exp.ae(dims=dims, patience=30, augmented_training=True, aug_rate_idx=1)
+        exp.ae(dims=dims, patience=30, augmented_training=True, aug_rate_idx=0)
         exp.classify()
-        exp.classify_with_DBG(aug_rate_idx=1)
+        exp.classify_with_DBG(aug_rate_idx=0)
 
     if args.expname == "FS-AE-DBG":
         # AE and Aug
